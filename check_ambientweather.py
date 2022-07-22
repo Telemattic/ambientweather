2d94f58509b1910d742ea36d99566851328f3d42
Matthew Eldridge <matthew.eldridge@gmail.com>
2022-07-22 08:31:21 -0700

    Better details

--- check_ambientweather.py
+++ check_ambientweather.py
@@ -1,7 +1,8 @@
 import datetime
+import json
 import mysql.connector
 
-def check_haversack():
+def check_ambientweather():
 
     ts = datetime.datetime.now()
 
@@ -10,18 +11,33 @@ def check_haversack():
 
     cursor = cnx.cursor()
 
-    cursor.execute("SELECT `time` FROM `weather` ORDER BY `id` DESC LIMIT 1")
+    cursor.execute("SELECT `json` FROM `weather` ORDER BY `id` DESC LIMIT 1")
+
+    curr = None
     for row in cursor:
-        dt = row[0]
+        curr = row[0]
         
     cursor.close()
     cnx.close()
 
-    delta = (datetime.datetime.now() - dt).total_seconds()
+    try:
+        curr = json.loads(curr)[0]['lastData']
+    except:
+        curr = None
+
+    if curr is None:
+        print("AmbientWeather CRITICAL - can't parse most recent data")
+        return 2
+
+    dt = curr['date'].replace('Z', '+00:00')
+    dt = datetime.datetime.fromisoformat(dt)
+
+    delta = (datetime.datetime.now(datetime.timezone.utc) - dt).total_seconds()
+    details = f"{curr['tempf']:.1f}F / {curr['tempinf']:.1f}F"
 
     interval = 60
     (status, ret) = ('OK', 0) if delta < interval * 2 else ('WARNING', 1)
-    print(f"AmbientWeather {status} - last {dt.isoformat()} ({delta:.0f}s)")
+    print(f"AmbientWeather {status} - last {dt.astimezone().isoformat()} ({delta:.0f}s) - {details}")
 
     return ret
 
@@ -29,7 +45,7 @@ def main():
 
     ret = 2
     try:
-        ret = check_haversack()
+        ret = check_ambientweather()
     except Exception as err:
         print(f"AmbientWeather CRITICAL - exception caught {err=} {type(err)=}")
         
